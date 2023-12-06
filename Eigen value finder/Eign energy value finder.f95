c15dbccdc8dc8bd43a8f36700fc44c49f985b21b
Sourabh <sourabhsheokand945@gmail.com>
2023-12-06 22:15:58 +0530

    modified:   Electron cloud/orbit.f95
    	new file:   Solution of HMI/Electron Cloud of HMI.png
    	modified:   Solution of HMI/main
    	modified:   Solution of HMI/main.f95
    	new file:   Solution of HMI/orbit.dat
    	new file:   Solution of HMI/secert.txt
    
    	modified:   Eigen value finder/Eign energy value finder.f95
    	modified:   Eigen value finder/full
    	new file:   Eigen value finder/output.txt

--- Eigen value finder/Eign energy value finder.f95
+++ Eigen value finder/Eign energy value finder.f95
@@ -1,108 +1,92 @@
 program main 
    implicit none
+
+   ! here we are only find the energy eigenvalue by using the shooting method and for this we 
+   ! we go from a range of energy and solve the radial equation for all the value of the energy and 
+   ! when the solution of radial equation behave like we wanted then we print this energy value and 
+   ! use the value to getting a very good radial and angular solution and also the electron cloud 
    
-   double precision , allocatable :: x(:) , si(:) , si1(:) , realsi1(:)
-   integer :: num_of_intervals , j , last_point , k 
-   double precision :: x_upper , x_lower , h , energy, step_size , energy_vibrator , normalizer , pot_energy , max_si , c2 , d , c_s
-   double precision :: energy_upper , energy_step
-   logical :: f1 , peak = .false. , zero = .false. , angular=.false.
-   print*, "Enter the distance " 
-   read*, d
+    double precision , allocatable :: x(:) , si(:) , si1(:) 
+    integer :: num_of_intervals , j , k 
+    double precision :: x_upper , x_lower , h , energy, step_size  , normalizer , c2 , d , c_s
+    double precision :: energy_upper , energy_step
+    logical :: f1 , zero = .false. 
+
+    print*, "Enter the distance "  ! here we getting the inter nuclear distance 
+    read*, d
+    
+    print*, "Enter the value of the energy : "  ! upper limit of the energy 
+    read*, energy_upper 
+                                        ! here we divide energy into 200 steps ,u can increase it 
+    energy_step = energy_upper/200    ! if u have a very good computer
+    
+    inquire(file="points.dat",exist=f1) ! opening the file for the store the data for plot 
+    if(f1) then
+    open(1,file="points.dat",status="replace")
+    else
+    open(1,file="points.dat",status="new",action="write")
+    endif
    
-   print*, "Enter the value of the energy : " 
-   read*, energy_upper 
+    do k = 1,200  ! this is our main loop for the energy eigenvalue
    
-   energy_step = energy_upper/200
+        energy = -energy_step*k ! calculating the energy c2 and c_s
+        
+        c2 = d*d*energy/4.0
+        c_s = 0.6043499*c2 - 0.006188*c2*c2 - 0.0000589*c2*c2*c2 +2 
+        !c_s = 0.3127477*c2 - 0.0231669*c2*c2 - 0.0005110*(c2**3) - 0.0000045*(c2**4) 
+
+        num_of_intervals = 10000 ! it for the radial equation 
+        x_upper = 10
+        x_lower = 1 + 1.0e-7
+        h = (x_upper - x_lower)/real(num_of_intervals)
+        
+        allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
+        
+        x(1) = x_lower  ! all the initial values and boundary condition
+        si(1) = 1
+        si1(1) = -(2*d + c2 - c_s)/2.0
+        normalizer = 0   ! here we also normalize the function 
+        do j = 2,num_of_intervals
+            call rkmethod(j)       ! here we calculate the radial function for given energy
+            normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
+        enddo    ! we use Trapezoidal method for integration 
+        
+        normalizer = sqrt(normalizer)
+        do j = 1, num_of_intervals
+            si(j) = si(j)/normalizer   ! in this condition we calculate that the function is 
+            if((abs(si(j)) < 1.0e-4 ) .and. (abs(si(j-10)) < 1.0e-4)) zero=.true. 
+            write(1,*)x(j),si(j),si1(j),si(j)**2 , energy  ! dying or not when go away 
+        enddo                          ! if the function is dying then we get the energy value 
+        write(1,*)""                   ! we print it's energy and if it's not then repeat for another value
+        if (zero) then 
+            print*, "The solution of the equation is the founded at " , energy , " and at ", k , "point"
+            deallocate(x,si,si1)
+            stop
+        endif
+        deallocate(x,si,si1)
+    enddo
    
-   inquire(file="points.dat",exist=f1) ! opening the file for the store the data for plot 
-   if(f1) then
-   open(1,file="points.dat",status="replace")
-   else
-   open(1,file="points.dat",status="new",action="write")
-   endif
-   
-   do k = 1,200
-   
-   energy = -energy_step*k
-   
-   
-   c2 = d*d*energy/4.0
-   c_s = 0.6043499*c2 - 0.006188*c2*c2 - 0.0000589*c2*c2*c2 +2 
-   !c_s = 0.3127477*c2 - 0.0231669*c2*c2 - 0.0005110*(c2**3) - 0.0000045*(c2**4) 
-   print*, c_s
-   
-   
-   num_of_intervals = 10000
-   x_upper = 10
-   x_lower = 1 + 1.0e-7
-   h = (x_upper - x_lower)/real(num_of_intervals)
-   
-   allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
-   
-   x(1) = x_lower
-   si(1) = 1
-   si1(1) = -(2*d + c2 - c_s)/2.0
-   normalizer = 0 
-   last_point = num_of_intervals
-   do j = 2,num_of_intervals
-       call rkmethod(j) 
-       normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
-       
-   enddo
-   
-   normalizer = sqrt(normalizer)
-   print*, normalizer
-   do j = 1, num_of_intervals
-       si(j) = si(j)/normalizer
-       if((abs(si(j)) < 1.0e-4 ) .and. (abs(si(j-10)) < 1.0e-4)) zero=.true. 
-       write(1,*)x(j),si(j),si1(j),si(j)**2 , energy
-       
-   enddo
-   write(1,*)""
-   if (zero) then 
-   	print*, "The solution of the equation is the founded at " , energy , " and at ", k , "point"
-   	deallocate(x,si,si1)
-   	stop
-   endif
-   deallocate(x,si,si1)
-   
-   enddo
-   
-   close(1)
+    close(1)
+    print*,'simulation is end'
+    stop
+    
+    contains
    	
-   print*,'simulation is end'
-   stop
-   
-   contains
-   	subroutine potential_energy_cal(po_energy,val_x)
-   	    double precision , intent(out) :: po_energy 
-   	    double precision, intent(in) :: val_x
-   	    po_energy = (0.765625e-2)*(val_x**2)
-   	    return 
-   	end subroutine potential_energy_cal
-   	
-   	double precision function func(val_x,val_y,val_y1) ! here the ODE we wanted to solve 
+   	    double precision function func(val_x,val_y,val_y1) ! here the radial equation 
             double precision , intent(in) :: val_x,val_y,val_y1
             double precision :: result , mid_1 , mid_2
-            if(angular) then
-            	mid_1 = (c2*val_x*val_x - c_s)*val_y
-		mid_2 = (2*val_x*val_y1)
-		result = (mid_1 + mid_2)/(1 - val_x*val_x)
-	    else
-		 mid_1 = ((2*d*val_x + c2*val_x*val_x - c_s)*val_y)
-		 mid_2 = 2*val_x*val_y1
-		 result = (-mid_1 - mid_2)/(val_x*val_x - 1)
-	    endif
-            !mid_1 = ((2*d*val_x + c2*val_x*val_x - c_s)*val_y)
-            !mid_2 = 2*val_x*val_y1
-            !result = (-mid_1 - mid_2)/(val_x*val_x - 1)
+            mid_1 = ((2*d*val_x + c2*val_x*val_x - c_s)*val_y)
+            mid_2 = 2*val_x*val_y1
+            result = (-mid_1 - mid_2)/(val_x*val_x - 1)
             func = result 
             return 
         end function func
+ 
+        ! here we calculate the value by the rk method and this is for 2nd order equation
 
-        subroutine rkmethod(i)   ! here we calculate the value by the rk method and this is for 2nd order equation 
+        subroutine rkmethod(i)    ! here we use 4th order rk method for 2nd order ODE
             integer , intent(in) :: i
-            double precision :: k1,k2,k3,k4,k11,k12,k13,k14,result  ! here we use 4th order rk method for 2nd order ODE
+            double precision :: k1,k2,k3,k4,k11,k12,k13,k14,result  
             k11 = h*si1(i-1)
             k1 = h*func(x(i-1),si(i-1),si1(i-1))
             k12 = (si1(i-1) + k1/2.0)*h
@@ -117,4 +101,3 @@ program main
             return 
         end subroutine rkmethod
 end program 
-   
--- Solution of HMI/main.f95
+++ Solution of HMI/main.f95
@@ -1,112 +1,124 @@
 program main 
-   implicit none !declaring the variable for the program 
-   
-   double precision , allocatable :: x(:) , si(:) , si1(:) ! here we use for both radial and angular part 
-   integer :: num_of_intervals , j                         ! by just reallocate the array
-   double precision :: x_upper , x_lower , h , energy, normalizer , c2 , d , c_s 
-   logical :: f1 , f2 , angular = .false.
-   
-   d = 2.0 ! it is the inter nuclear distance 
-   
-   print*, "Enter the value of the energy : " 
-   read*, energy
-   
-   num_of_intervals = 10000
-   
-   c2 = d*d*energy/4.0  ! c_s can be taken by both value it depend upon the what type of system it is.
-   c_s = 0.6043499*c2 - 0.006188*c2*c2 - 0.0000589*c2*c2*c2 +2
-   !c_s = 0.3127477*c2 - 0.0231669*c2*c2 - 0.0005110*(c2**3) - 0.0000045*(c2**4) 
-   
-   ! for radial part solution 
-   
-   inquire(file="radial.dat",exist=f1) ! opening the file for the store the data for plot 
-   if(f1) then
-   	open(1,file="radial.dat",status="replace")
-   else
-   	open(1,file="radial.dat",status="new",action="write")
-   endif
-   
+implicit none !declaring the variable for the program 
 
-   x_upper = 10         ! This is the upper limit we can extent it also for higher energies
-   x_lower = 1 + 1.0e-7 ! we can't use the 1 because the function is not defined at 1
-   h = (x_upper - x_lower)/real(num_of_intervals)
-   
-   allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
-   
-   ! here x = e , si = R(e) , si1 = R'(e)
+double precision , allocatable :: x(:) , si(:) , si1(:) ! here we use for both radial and angular part 
+double precision :: prob(10)
+integer :: num_of_intervals , j  ,count = 0             ! by just reallocate the array
+double precision :: x_upper , x_lower , h , energy, normalizer , c2 , d , c_s 
+logical :: f1 , f2 , angular = .false.
+real , parameter :: pi = 3.141592653589793
 
-   x(1) = x_lower
-   si(1) = 1
-   si1(1) = -(2*d + c2 - c_s)/2.0 ! this is the boundary condition we derive with m=0
-   normalizer = 0  ! normalizer to use to normalize the function it use Trapezoidal method integration
+d = 2.0 ! it is the inter nuclear distance 
 
-   do j = 2,num_of_intervals
-       call rkmethod(j)       ! here we call the rk method to solve for next value 
-       normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
-   enddo
-   
-   normalizer = sqrt(2*normalizer) ! we multiply it with 2 because we find only one side of the radial fxn
-                                   ! And for we getting exact value when we do for both side 
-  
-   do j = 1, num_of_intervals
-       si(j) = si(j)/normalizer
-       write(1,*)x(j),si(j),si1(j),si(j)**2,si(j)*x(j)
-   enddo
-   
-   deallocate(x,si,si1) ! here we deallocate the array for use in angular part 
-   
-   close(1)
-   
-   ! for Angular wave function solution 
-   
-   angular = .true.  ! this change the func into angular equation
-   
-   inquire(file='angular.dat',exist=f2)
-   if(f2) then 
-   	open(2,file="angular.dat",status="replace")
-   else
-   	open(2,file="angular.dat",status="new",action="write")
-   endif
-   ! x = n , si = S(n) , si1 = S'(n)
-   allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
-   
-   x_upper = 0    ! here we done for half of the and generate the other value using these values
-   x_lower = -1 + 1.0e-4  
-   h = (x_upper - x_lower)/real(num_of_intervals)
-   
-   x(1) = x_lower
-   si(1) = 1
-   si1(1) = (c2-c_s)/2  ! here m = 0 and we get this equation in Boundary condition section 
-   normalizer = 0       ! normalizer is same as the radial part but we not using it.
- 
-   do j = 2,num_of_intervals
-       call rkmethod(j) 
-       normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
-   enddo
-   
-   normalizer = sqrt(2*normalizer)
-   do j = 1,num_of_intervals       ! this gives value between -1 to 0 
-      !si(j) = si(j)/normalizer
-       write(2,*)x(j),si(j),si1(j)/normalizer,si(j)/x(j)
-   enddo
-  do j = num_of_intervals,1,-1    ! here we generate the value b/w 0 to 1 
-      !si(j) = si(j)/normalizer   ! using the other values 
-      write(2,*)-x(j),si(j),si1(j)/normalizer,si(j)/x(j)
-   enddo
-   
-   deallocate(x,si,si1) ! here we finally deallocate the memory
-   close(2)
-   	  
-   print*,'simulation is end'
-   stop
-   
-   contains  ! func is the wave equations
+print*, "Enter the value of the energy : " 
+read*, energy
 
-   	    double precision function func(val_x,val_y,val_y1)  
+num_of_intervals = 10000
+
+c2 = d*d*energy/4.0  ! c_s can be taken by both value it depend upon the what type of system it is.
+c_s = 0.6043499*c2 - 0.006188*c2*c2 - 0.0000589*c2*c2*c2 +2
+!c_s = 0.3127477*c2 - 0.0231669*c2*c2 - 0.0005110*(c2**3) - 0.0000045*(c2**4) 
+
+! for radial part solution 
+
+inquire(file="radial.dat",exist=f1) ! opening the file for the store the data for plot 
+if(f1) then
+    open(1,file="radial.dat",status="replace")
+else
+    open(1,file="radial.dat",status="new",action="write")
+endif
+
+
+x_upper = 10         ! This is the upper limit we can extent it also for higher energies
+x_lower = 1 + 1.0e-7 ! we can't use the 1 because the function is not defined at 1
+h = (x_upper - x_lower)/real(num_of_intervals)
+
+allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
+
+! here x = e , si = R(e) , si1 = R'(e)
+
+x(1) = x_lower
+si(1) = 1
+si1(1) = -(2*d + c2 - c_s)/2.0 ! this is the boundary condition we derive with m=0
+normalizer = 0  ! normalizer to use to normalize the function it use Trapezoidal method integration
+
+do j = 2,num_of_intervals
+    call rkmethod(j)       ! here we call the rk method to solve for next value 
+    normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
+enddo
+
+normalizer = sqrt(2*normalizer) ! we multiply it with 2 because we find only one side of the radial fxn
+                                ! And for we getting exact value when we do for both side 
+
+do j = 1, num_of_intervals
+
+    if (j == 1000*count+1) then  ! the prob is use for make the electron cloud 
+        prob(count+1) = si(j)    ! here prob store the value at the some points
+        count = count + 1        ! that have max in range we can sum for all value 
+    endif                        ! but i choose this way and it works fine
+
+    si(j) = si(j)/normalizer
+    write(1,*)x(j),si(j),si1(j),si(j)**2,si(j)*x(j)
+enddo
+
+deallocate(x,si,si1) ! here we deallocate the array for use in angular part 
+
+close(1)
+
+! for Angular wave function solution 
+
+angular = .true.  ! this change the func into angular equation
+
+inquire(file='angular.dat',exist=f2)
+if(f2) then 
+    open(2,file="angular.dat",status="replace")
+else
+    open(2,file="angular.dat",status="new",action="write")
+endif
+! x = n , si = S(n) , si1 = S'(n)
+allocate(x(num_of_intervals+1),si(num_of_intervals+1),si1(num_of_intervals+1))
+
+x_upper = 0    ! here we done for half of the and generate the other value using these values
+x_lower = -1 + 1.0e-4  
+h = (x_upper - x_lower)/real(num_of_intervals)
+
+x(1) = x_lower
+si(1) = 1
+si1(1) = (c2-c_s)/2  ! here m = 0 and we get this equation in Boundary condition section 
+normalizer = 0       ! normalizer is same as the radial part but we not using it.
+
+do j = 2,num_of_intervals
+    call rkmethod(j) 
+    normalizer = normalizer + 0.5*h*(si(j)*si(j) + si(j-1)*si(j-1))
+enddo
+
+normalizer = sqrt(2*normalizer)
+do j = 1,num_of_intervals       ! this gives value between -1 to 0 
+    !si(j) = si(j)/normalizer
+    write(2,*)x(j),si(j),si1(j)/normalizer,si(j)/x(j)
+enddo
+do j = num_of_intervals,1,-1    ! here we generate the value b/w 0 to 1 
+    !si(j) = si(j)/normalizer   ! using the other values 
+    write(2,*)-x(j),si(j),si1(j)/normalizer,si(j)/x(j)
+enddo
+
+deallocate(x,si,si1) ! here we finally deallocate the memory
+close(2)
+    
+    ! Making the electron cloud by calling the subroutine orbit 
+
+call orbit(prob)
+
+print*,'simulation is end'
+stop
+
+contains  ! func is the wave equations
+
+        double precision function func(val_x,val_y,val_y1)  
             double precision , intent(in) :: val_x,val_y,val_y1
             double precision :: result , mid_1 , mid_2 
             if(angular) then    
-            	mid_1 = (c2*val_x*val_x - c_s)*val_y ! angular function 
+                mid_1 = (c2*val_x*val_x - c_s)*val_y ! angular function 
                 mid_2 = (2*val_x*val_y1)
                 result = (mid_1 + mid_2)/(1 - val_x*val_x)
                 else
@@ -117,7 +129,8 @@ program main
                     func = result
             return 
         end function func
-                                ! here we calculate the value by the rk method and this is for 2nd order equation
+
+        ! here we calculate the value by the rk method and this is for 2nd order equation
         subroutine rkmethod(i)    
             integer , intent(in) :: i
             double precision :: k1,k2,k3,k4,k11,k12,k13,k14,result  
@@ -134,5 +147,45 @@ program main
             si1(i) = si1(i-1) + (k1+2*k2+2*k3+k4)/6.0
             return 
         end subroutine rkmethod
+
+        ! the logic behind this program is simple we just calculate the radial equation 
+        ! Because i am not good in file operation in FORTRAN  and i am still learning
+        ! so i just calculate the radial distribution and from that calculate the probability
+        ! finding the electron and multiply it with 100 or any other number and generate the 
+        ! resultant number random numbers and plot and we get the electron cloud 
+
+        subroutine orbit(probability)
+            double precision , dimension(10) , intent(in):: probability
+            integer :: i,k,points
+            double precision :: t,r,x1,y1 
+            logical :: f2 
+
+            inquire(file="orbit.dat",exist=f2) 
+            if(f2) then 
+                open(3,file="orbit.dat",status="replace")
+            else 
+                open(3,file="orbit.dat",status="new",action="write")
+            endif
+
+            do k = 1,5
+                points = probability(k)*100
+                do i = 1, 1000
+                    if (points < i) then ! here we generate the random no. in circle like 
+                        call random_number(t)  ! the random position of electron and as the 
+                        call random_number(r)  ! probability of find the electron increase the 
+                        r = r*real(k)          ! the number of the points are also increasing 
+                        x1 = r*cos(t*2*pi) + 1 ! and we get what we wanted and repeat this 
+                        y1 = r*sin(2*pi*t)     ! process for both the protons 
+                        write(3,*)x1,y1
+                        x1 = r*cos(t*2*pi) - 1
+                        y1 = r*sin(2*pi*t) 
+                        write(3,*)x1,y1
+                    endif
+                enddo
+                write(3,*)""
+            enddo
+            close(3)
+            return 
+        end subroutine orbit
 end program 
-   
+
